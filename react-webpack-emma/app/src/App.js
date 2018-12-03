9a4a5fabeebbf0477bb56f686e4a4a9d1f95309b
Eoan O'Dea <odeaeoan@gmail.com>
2018-12-03 13:38:23 +0100

    normalize & resets

--- react-webpack-emma/app/src/App.js
+++ react-webpack-emma/app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './sass/style.min.css';
 import Spotify from 'spotify-web-api-js';
 import SpotifyIcon from '../src/images/Spotify.png';
 
