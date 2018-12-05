3f526d13e937e7833c7b7571c402454103bf26fc
MaximWuyts <32163906+MaximWuyts@users.noreply.github.com>
2018-12-05 19:51:31 +0100

    fixed

--- react-webpack-emma/app/src/components/MainNavigation_Component.js
+++ react-webpack-emma/app/src/components/MainNavigation_Component.js
@@ -1,26 +1,16 @@
 import React from 'react';
 
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from 'react-router-dom';
-
 
 
 export class MainNavigation extends React.Component {
-    render = () => {
+    render() {
         return (
             <div>
                 <header>
-
                     <nav>
-                        <Router>
-                            <button><Link to="http://localhost:8888/login">Login to Spotify</Link></button>
-                            <button onClick={() => this.getNowPlaying()}>Check now playing</button>
-                            <div className="divider"></div>
-                        </Router>
+                        <button href="http://localhost:8888/login">Login to Spotify</button>
+                        <button onClick={() => this.getNowPlaying()}>Check now playing</button>
+                        <div className="divider"></div>
                     </nav>
                 </header>
             </div>
