bfc5dd10db83775e29236dc312e0408f67f17fbb
Eoan O'Dea <odeaeoan@gmail.com>
2018-12-11 20:25:07 +0100

    Attempting to fetch user profile info

--- react-webpack-emma/app/src/components/MainNavigation.Component.js
+++ react-webpack-emma/app/src/components/MainNavigation.Component.js
@@ -1,14 +1,57 @@
 import React from 'react';
+import Spotify from 'spotify-web-api-js';
 import SpotifyIcon from '../images/Spotify.png';
 
+const spotifyWebApi = new Spotify();
+
 export class MainNavigation extends React.Component {
+    constructor() {
+        super();
+        const params = this.getHashParams();
+        this.state = {
+            loggedIn: params.access_token ? true : false,
+            nowPlaying: {
+                song: '',
+                image: ''
+            }
+        }
+        if (params.access_token) {
+            spotifyWebApi.setAccessToken(params.access_token)
+        }
+
+        this.profile = this.profile.bind(this);
+    }
+
+    getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        while (e = r.exec(q)) {
+            hashParams[e[1]] = decodeURIComponent(e[2]);
+        }
+        return hashParams;
+    }
+    
     
     profile() {
+        spotifyWebApi.getMyCurrentPlaybackState().then((response) => {
+            this.setState({
+                nowPlaying: {
+                    song: response.item.name,
+
+                    //user profile picture to be added here
+                }
+            })
+
+        })
+
         let profile = false;
         if(window.location.href.includes('#access_token')) {
             profile = true;
             return (
-                <h1></h1>// <h1>{this.props.myFunction.nowPlaying.song}</h1>
+                <span>
+                </span>
+                //this.state.nowPlaying.song
             );
         } else {
             profile = false;
