e8eaa6afb9930f5f958345fdc3f5b03e46ddd669
MaximWuyts <32163906+MaximWuyts@users.noreply.github.com>
2018-12-23 23:15:33 +0100

    first setup

--- react-webpack-emma/app/src/App.js
+++ react-webpack-emma/app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './scss/style.min.css';
 import { HomePage } from '../src/components/HomePage.Component';
 import { Playlist } from '../src/components/Playlist.Component';
+import { Profile } from '../src/components/Playlist.Component';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
                     <div>
                         <Route exact path="/" component={HomePage} />
                         <Route path="/playlist" component={Playlist} />
+                        <Route path="/profile" component={Profile} />
                     </div>
                 </BrowserRouter>
 
