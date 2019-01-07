218f4fbd67d8499a827b47ee644bd167f43109a4
MaximWuyts <32163906+MaximWuyts@users.noreply.github.com>
2019-01-07 20:25:33 +0100

    try edit route

--- react-webpack-emma/server/routes/api/items.js
+++ react-webpack-emma/server/routes/api/items.js
@@ -32,8 +32,35 @@ router.get('/:id', (req, res) => {
         .catch(err => res.status(404).json({ success: false }));
 })
 
+//edit a song
+/*router.put('/:id', (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.update(item).then(() => res.json({ success: true })))
 
+    item.save()
+        .then(item => res.json(item));
+});
 
+*/
+/*
+router.put('/:id', (req, res) => {
+    const item = req.body;
+    const newItem = {};
+
+    if (item.artist) {
+        newItem.artist = item.artist;
+    }
+    if (!newItem) {
+        res.status(400);
+        res.json({
+            error: "bad Data"
+        });
+    }
+    else {
+        Item.findById(req.params.id).update(newItem).then(() => res.json({ success: true }))
+    }
+})
+*/
 
 // @route delte api/items/:id
 // delete a song
